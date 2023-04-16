c77d3193318d923ac74f0063d56308f7509879a9
SeMsei <fomichevsjiy@gmail.com>
2023-04-16 22:59:35 +0300

    added get methods in db_manager

--- Project_managment_tool/db_manager.h
+++ Project_managment_tool/db_manager.h
@@ -8,11 +8,12 @@
 #include <sstream>
 #include <map>
 #include "employee.h"
+#include "personaltask.h"
 #include "role.h"
 #include <iostream>
 #include "worktask.h"
-/*#include "personaltask.h"
-#include "project.h"*/
+#include "personaltask.h"
+#include "project.h"
 
 class Employee;
 class WorkTask;
@@ -106,7 +107,145 @@ public:
         return ret_vec;
     }
 
+    static std::vector<WorkTask> get_worktasks(std::string connection_str) {
+        std::vector<WorkTask> ret_vec;
+        std::ifstream in(connection_str + "worktasks.txt");
+        std::string line;
 
+        while (std::getline(in, line)) {
+            //id, name, is_done, date_start, date_finish, is_overdue, project_id
+            //employees_id
+
+            int id, tmp_id, project_id;
+            bool is_done, is_overdue;
+            std::string name, date_s_str, date_f_str;
+            std::stringstream cont;
+            std::chrono::year_month_day date_start, date_finish;
+
+            cont << line;
+            cont >> id >> name >> is_done >> date_s_str >> date_f_str >> is_overdue >> project_id;
+
+            std::istringstream iss_s{date_s_str}, iss_f{date_f_str};
+            std::chrono::from_stream(iss_s, "%F", date_start);
+            std::chrono::from_stream(iss_f, "%F", date_finish);
+
+            WorkTask tmp(id, name, date_start, date_finish, project_id);
+
+            if (is_done) {
+                tmp.complete();
+            }
+
+            tmp.change_overdue_status(is_overdue);
+
+            std::getline(in, line);
+            cont.clear();
+            cont << line;
+
+            while (cont >> tmp_id) {
+                tmp.add_employee(tmp_id);
+            }
+
+            ret_vec.push_back(WorkTask(tmp));
+
+        }
+
+        return ret_vec;
+    }
+
+    static std::vector<PersonalTask> get_personaltasks(std::string connection_str) {
+        std::vector<PersonalTask> ret_vec;
+        std::ifstream in(connection_str + "personaltasks.txt");
+        std::string line;
+
+        while (std::getline(in, line)) {
+            //id, name, is_done, date_start, date_finish, is_overdue, employee_id
+            //description
+
+            int id, employee_id;
+            bool is_done, is_overdue;
+            std::string name, date_s_str, date_f_str;
+            std::stringstream cont;
+            std::chrono::year_month_day date_start, date_finish;
+
+            cont << line;
+            cont >> id >> name >> is_done >> date_s_str >> date_f_str >> is_overdue >> employee_id;
+
+            std::istringstream iss_s{date_s_str}, iss_f{date_f_str};
+            std::chrono::from_stream(iss_s, "%F", date_start);
+            std::chrono::from_stream(iss_f, "%F", date_finish);
+
+            PersonalTask tmp(id, name, date_start, date_finish, employee_id);
+
+            if (is_done) {
+                tmp.complete();
+            }
+
+            tmp.change_overdue_status(is_overdue);
+
+            std::getline(in, line);
+
+            tmp.set_description(line);
+
+            ret_vec.push_back(PersonalTask(tmp));
+
+        }
+
+        return ret_vec;
+    }
+
+    static std::vector<Project> get_projects(std::string connection_str) {
+        std::vector<Project> ret_vec;
+        std::ifstream in(connection_str + "projects.txt");
+        std::string line;
+
+        while (std::getline(in, line)) {
+            //id, name, date_start, date_finish, task_count, dont_task_count, is_done,
+            //employee_vec
+            //task_vec
+            int id, tmp_id, task_count, done_task_count;
+            std::string name;
+            bool is_done;
+            std::chrono::year_month_day date_start, date_finish;
+            std::string date_start_str, date_finish_str;
+            std::stringstream cont;
+
+            cont << line;
+            cont >> id >> name >> date_start_str >> date_finish_str >> task_count >> done_task_count >> is_done;
+
+            std::istringstream iss_s{date_start_str}, iss_f{date_finish_str};
+            std::chrono::from_stream(iss_s, "%F", date_start);
+            std::chrono::from_stream(iss_f, "%F", date_finish);
+
+            Project tmp(id, name, date_start, date_finish);
+
+            for (int i = 0; i < done_task_count; i++)
+                tmp.complete_task();
+
+            if (is_done)
+                tmp.complete();
+
+            cont.clear();
+            std::getline(in, line);
+            cont << line;
+
+            while (cont >> tmp_id)
+                tmp.add_employee(tmp_id);
+
+            cont.clear();
+            std::getline(in, line);
+            cont << line;
+
+            while (cont >> tmp_id)
+                tmp.add_task(tmp_id);
+
+
+
+            ret_vec.push_back(Project(tmp));
+
+        }
+
+        return ret_vec;
+    }
 };
 
 #endif // DB_MANAGER_H
